639ce382b7ef4ed9ad507bd07b466eacd41b0fc8
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 12:37:40 -0500

    add range output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
-echo "::set-output name=minimum::$minimum"
-latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r 'split(".") | .[0] + "." + .[1]')
-echo "::set-output name=latest::${latest}"
+Minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
+echo "::set-output name=minimum::$Minimum"
+
+Latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r 'split(".") | .[0] + "." + .[1]')
+echo "::set-output name=latest::${Latest}"
+
+echo "::set-output name=range::[${Minimum}, ${Latest}]"
