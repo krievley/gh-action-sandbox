a66254ef853d7c49b8f282ce735baceff46eae70
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 09:57:00 -0500

    update output names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 minimum=$(curl --url 'https://phpreleases.com/api/releases/minimum-supported/active')
-echo "::set-output name=releases::$minimum"
+echo "::set-output name=minimum::$minimum"
 latest=$(curl --url 'https://phpreleases.com/api/releases/latest')
-echo "::set-output name=releases::$latest%.*"
+echo "::set-output name=latest::$latest%.*"
