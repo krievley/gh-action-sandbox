bca6a20b2bd9e60d77a14159580c3201f8f6688f
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 09:07:20 -0500

    add flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-releases=$(curl https://phpreleases.com/api/releases/latest -header "Accept=application/json")
+releases=$(curl --url 'https://phpreleases.com/api/releases/latest')
 echo $releases
 echo "::set-output name=releases::$releases"
