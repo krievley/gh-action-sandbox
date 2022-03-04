bf6099c166bf0258aeb2886b4e5610370f6045e6
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 08:59:08 -0500

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-releases=$(curl 'https://phpreleases.com/api/releases/latest')
+releases=$(curl https://phpreleases.com/api/releases/latest -header "Accept=application/json")
+echo $releases
 echo "::set-output name=releases::$releases"
