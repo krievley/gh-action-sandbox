942c658c72a6dcad46b321e8147f87a536abc0e1
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 11:05:53 -0500

    remove redundant string manipulation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
 echo "::set-output name=minimum::$minimum"
 latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r 'split(".") | .[0] + "." + .[1]')
-echo "::set-output name=latest::${latest%.*}"
+echo "::set-output name=latest::${latest}"
