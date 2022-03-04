5a86d7c07b87c0692ee93a6885002621a88234f4
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 11:03:52 -0500

    fix formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
 echo "::set-output name=minimum::$minimum"
-latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r split(".") | .[0] + "." + .[1])
+latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r 'split(".") | .[0] + "." + .[1]')
 echo "::set-output name=latest::${latest%.*}"
