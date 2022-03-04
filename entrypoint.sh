3ebc167a5c2225375948a07bb942c4a1d9f1229e
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 10:32:55 -0500

    convert numbers to strings for concatenation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq '.major + "." + .minor')
+minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq '.major|toString + "." + .minor|toString')
 echo "::set-output name=minimum::$minimum"
 latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r)
 echo "::set-output name=latest::${latest%.*}"
