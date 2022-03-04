27338c2be59be482a4b28150353267c4ddcc2427
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 11:02:56 -0500

    try something else

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '.[] | "\(.major)\.(.minor)"')
+minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major|tostring) + "." + (.minor|tostring)')
 echo "::set-output name=minimum::$minimum"
-latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest')
-echo "::set-output name=latest::${latest%.*}"\""
+latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest' | jq -r split(".") | .[0] + "." + .[1])
+echo "::set-output name=latest::${latest%.*}"
