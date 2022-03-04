ca5733d2bba9b02cfa5912a69dd42a325807968d
faxblaster <alison.l.kirk@gmail.com>
2022-03-04 10:20:21 -0500

    more js attempts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '.major, .minor')
+minimum=$(curl -k --url 'https://phpreleases.com/api/releases/minimum-supported/active' | jq -r '(.major + "." + .minor)'
 echo "::set-output name=minimum::$minimum"
 latest=$(curl -k --url 'https://phpreleases.com/api/releases/latest')
-echo "::set-output name=latest::$latest"
+echo "::set-output name=latest::${latest%.*}"
